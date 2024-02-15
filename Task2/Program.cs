e59beb079c6e7df8fd83045a9e266be0ffec80db
Alexander Malyatin <paprikka84@gmail.com>
2024-02-16 00:12:19 +0600

    Lesson1:Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает целое число N, а на выходе 
+// показывает все целые числа в промежутке от -N до N.
+
+Console.Write("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 0)
+{
+  num = num*(- 1);
+}
+int index = - num;
+while (index <= num)
+{
+  Console.Write($"{index} ");
+  index++;
+}
