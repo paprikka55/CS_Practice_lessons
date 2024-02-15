58cd74f182949790fe0c940f66e3dfc25ae95803
Alexander Malyatin <paprikka84@gmail.com>
2024-02-16 01:22:25 +0600

    add Lesson1:Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите целое трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = num % 10;
+int num2 = num / 100;
+Console.WriteLine($"Сумма чисел {num1} и {num2} равна: {num1 + num2}");
