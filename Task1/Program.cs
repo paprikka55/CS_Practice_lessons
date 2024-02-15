d29f20a61d38f936d6a3d1b3eaf0ad629ac96471
Alexander Malyatin <paprikka84@gmail.com>
2024-02-15 23:47:14 +0600

    Lesson1:Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿//using System;
+// Напишите программу, которая на вход принимает 
+// два целых числа и проверяет, является ли первое число квадратом второго.
+Console.Write("Введите целое число №1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число №2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == Math.Pow(num2, 2))
+{
+  Console.WriteLine($"Условия выполнены! Квадрат числа {num2} является числом {num1}");
+}
+else
+{
+  Console.WriteLine($"Условия выполнены! Квадрат числа {num2} не является числом {num1}");
+}
